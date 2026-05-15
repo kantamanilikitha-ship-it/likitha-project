945df14ffb726b581478d4541e5c4c67af818a9b
Likitha <likitha@gmail.com>
2026-05-15 19:28:21 +0530

    Fix homepage route

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const path = require('path');
 const { initDb } = require('./database/db');
 
 const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
